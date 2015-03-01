b8c433e48792679fc57e02dbb98684dd6a4f0c3a
Alex Kalayjian <alexkalayjian@gmail.com>
2015-02-28 23:39:43 -0200

    - saco heads y cosas raras

--- Taller II/src/PruebaCliente2.java
+++ Taller II/src/PruebaCliente2.java
@@ -27,15 +27,9 @@ import logica.exceptions.ExceptionsPersistencia;
 
 
 public class PruebaCliente2 {
-<<<<<<< HEAD
-	public static void main(String[] args) throws NotBoundException,
-			ExceptionsJugadores, ExceptionsPeliculas,
-			ExceptionCodigoIncorrecto, IOException, ExceptionPartidas,
-			ClassNotFoundException, ExceptionsPersistencia {
-		
-=======
+
 	public static void main(String[] args) throws NotBoundException, ExceptionsJugadores, ExceptionsPeliculas, ExceptionCodigoIncorrecto, IOException, ExceptionPartidas, ClassNotFoundException, ExceptionsPersistencia {
->>>>>>> branch 'master' of https://github.com/alexkala/UDE-Taller-2.git
+
 		String url ="//" + ManageString.getProperty("ip")+ ":" + ManageString.getProperty("puerto")	+ "/" + ManageString.getProperty("nombre");		
 		IFachadaCapaLogica fachada = (IFachadaCapaLogica) Naming.lookup(url); 	// ACCEDE AL SERVER 
 		
